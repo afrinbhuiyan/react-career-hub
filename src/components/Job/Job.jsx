dbaff9ff6bb3d6acc8fc2a50837809b93015f9fb
afrinbhuiyan <mstafrinbhuiyan@gmail.com>
2024-11-30 22:31:36 +0600

    Add icons and handle show all jobs

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,9 +1,10 @@
 
 import './Job.css'
+import { TfiLocationPin } from "react-icons/tfi";
+import { PiCurrencyCircleDollar } from "react-icons/pi";
 const Job = ({ job }) => {
     const { logo, job_title, company_name, remote_or_onsite, location,
-        job_type, salary, job_description, job_responsibility, 
-     } = job;
+        job_type, salary} = job;
     return (
         <div className="bg-base-100 border rounded-lg shadow-xl">
             <figure className="px-10 pt-10">
@@ -19,6 +20,10 @@ const Job = ({ job }) => {
                 <div>
                     <button className="button mr-4 text-[#6c45f7b9]"> {remote_or_onsite} </button>
                     <button className='button text-[#6c45f7b9]'> {job_type} </button>
+                </div>
+                <div className='flex gap-3'>
+                    <h3 className='flex items-center font-semibold text-[#111111b9]' ><TfiLocationPin /> {location} </h3>
+                    <h3 className='flex items-center font-semibold text-[#111111b9] ' ><PiCurrencyCircleDollar /> {salary} </h3>
                 </div>
                 <div className="card-actions">
                     <button className="btn font-bold text-white bg-gradient-to-r from-sky-500 to-[#9245f7b9]">View Details</button>
